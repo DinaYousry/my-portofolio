d29b5388603db3ee8e63d2145c712ac070951458
hwaya2022 <97099477+hwaya2022@users.noreply.github.com>
2022-04-12 13:41:29 +0200

    1204-2022 01:41pm

---
+++ src/Asstet/Index.js
@@ -0,0 +1,4 @@
+export {default as logo} from './khyma.png'
+export {default as food} from './food.jpg'
+export {default as places} from './places.jpg'
+export {default as series} from './series.jpg'
