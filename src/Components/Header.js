b530ce60dd87cf3193086ffcb65862d81848314d
hwaya2022 <97099477+hwaya2022@users.noreply.github.com>
2022-04-12 12:59:16 +0200

    12-04-2022 12:59pm

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import {Navbar , Nav, Offcanvas ,Container } from 'react-bootstrap'
+import { Nav, Navbar ,Offcanvas , Container ,Col , Row } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
-
+<Container>
         <Navbar sticky="top" expand={false}>
   <Container fluid>
-    <Navbar.Brand href="#">D_Y</Navbar.Brand>
+    <Navbar.Brand href="/" id="offcanvasNavbarLabel">D_Y</Navbar.Brand>
     <Navbar.Toggle aria-controls="offcanvasNavbar" />
     <Navbar.Offcanvas
       id="offcanvasNavbar"
@@ -15,19 +16,73 @@ const Header = () => {
       placement="end"
     >
       <Offcanvas.Header closeButton>
-        <Offcanvas.Title id="offcanvasNavbarLabel">D_Y</Offcanvas.Title>
+        <Offcanvas.Title id="offcanvasNavbarLabel2">D_Y</Offcanvas.Title>
       </Offcanvas.Header>
+
       <Offcanvas.Body>
-        <Nav className="justify-content-end flex-grow-1 pe-3">
-          <Nav.Link href="#action1">Home</Nav.Link>
-          <Nav.Link href="#action2">Resume</Nav.Link>
-          <Nav.Link href="#action2">Projects</Nav.Link>
-          <Nav.Link href="#action2">Contact</Nav.Link>
+        <Nav >
+<br/><br/><br/><br/><br/>
+          <Row className='rowww'>
+                <Col lg={5} xs={5}>
+                <p className='par' >
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-house-door" viewBox="0 0 16 16">
+                <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"/>
+                </svg>
+                </p>
+                </Col>
+                <Col lg={7} xs={7}  style={{textAlign:"right"}}>
+                <NavLink className="navlink"  to="/">Home</NavLink>
+                </Col>
+            </Row>
+
+            <Row  className='rowww'>
+                <Col lg={5} xs={5}>
+                <p className='par' >
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-file-person" viewBox="0 0 16 16">
+                <path d="M12 1a1 1 0 0 1 1 1v10.755S12 11 8 11s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z"/>
+                <path d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                </svg>
+                </p>
+                </Col>
+                <Col lg={7} xs={7} style={{textAlign:"right"}}>
+                <NavLink className="navlink" to="/">Resume</NavLink>
+                </Col>
+            </Row>
+
+            <Row  className='rowww'>
+                <Col lg={5} xs={4}>
+                <p className='par' >
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-journal-code" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8.646 5.646a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L10.293 8 8.646 6.354a.5.5 0 0 1 0-.708zm-1.292 0a.5.5 0 0 0-.708 0l-2 2a.5.5 0 0 0 0 .708l2 2a.5.5 0 0 0 .708-.708L5.707 8l1.647-1.646a.5.5 0 0 0 0-.708z"/>
+                <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
+                <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
+                </svg>
+                </p>
+                </Col>
+                <Col lg={7} xs={8} style={{textAlign:"right"}}>
+                <NavLink className="navlink" to="/">Projects</NavLink>
+                </Col>
+            </Row>
+
+            <Row  className='rowww'>
+                <Col lg={5} xs={4}>
+                <p className='par' >
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-telephone-inbound-fill" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM15.854.146a.5.5 0 0 1 0 .708L11.707 5H14.5a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 1 0v2.793L15.146.146a.5.5 0 0 1 .708 0z"/>
+                </svg>
+                </p>
+                </Col>
+                <Col lg={7} xs={8} style={{textAlign:"right"}}>
+                <NavLink className="navlink" to="/"> Contact</NavLink>
+                </Col>
+            </Row>
+
         </Nav>
       </Offcanvas.Body>
     </Navbar.Offcanvas>
   </Container>
 </Navbar>
+</Container>
     </>
   )
 }
